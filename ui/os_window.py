20af0f268be4630636e36fc7a4f0ed25c7a8aee8
ricsix19 <ricsix19@mailbox.unideb.hu>
2025-08-23 16:15:46 +0200

    feat(ui): implement reusable OS-specific shutdown window

---
+++ ui/os_window.py
@@ -0,0 +1,56 @@
+import tkinter as tk
+from tkinter import messagebox
+from core import commands, platform_utils
+
+def create_os_windows(app, title, os_name):
+    app.withdraw()
+    window = tk.Toplevel(app)
+    window.title(title)
+    window.geometry("300x400")
+    window.resizable(False, False)
+
+    label = tk.Label(window, text = "Enter shutdown time (seconds):")
+    label.pack(pady=10)
+
+    timer_entry = tk.Entry(window, width = 50)
+    timer_entry.pack(pady=10)
+
+    def shutdown():
+        timer = timer_entry.get()
+        if not timer.isdigit():
+            messagebox.showerror("Invalid Number", "Enter a valid number")
+            return
+
+        if messagebox.askokcancel("Shutdown", "Do you really want to shutdown?"):
+            if platform_utils.ensure_os(os_name):
+                if os_name == "Windows":
+                    commands.shutdown_windows(timer)
+                else:
+                    commands.shutdown_linux(timer)
+            else:
+                platform_utils.show_worng_os_error()
+
+    def abort():
+        if messagebox.askokcancel("Abort shutdown", "Do you really want to abort shutdown?"):
+            if platform_utils.ensure_os(os_name):
+                commands.abort_windows() if os_name == "Windows" else commands.abort_linux()
+            else:
+                platform_utils.show_worng_os_error()
+
+    tk.Button(window, text="Shutdown", command=shutdown, height=2, width=20).pack(pady=10)
+    tk.Button(window, text="Abort shutdown", command=abort, height=2, width=20).pack(pady=15)
+    tk.Button(window, text="Exit", command=app.destroy, height=2, width=20).pack(pady=10)
+    tk.Button(window, text="Back", command=lambda: back(window, app), height=2, width=20).pack(pady=10)
+
+    center_window(window)
+
+def back(window, app):
+    window.destroy()
+    app.deiconify()
+
+def center_window(window):
+    window.update_idletasks()
+    width, height = window.winfo.width(), window.winfo.height()
+    x = (window.winfo_screenwidth() // 2) - (width // 2)
+    y = (window.winfo_screenheight() // 2) - (height // 2)
+    window.geometry(f"{width}x{height}+{x}+{y}")
