13975356af895916c497a4f0ebe2c7b299e0efe3
ricsix19 <ricsix19@mailbox.unideb.hu>
2025-08-24 16:43:54 +0200

    fix(ui): Fixed main window not appearing in the center of the screen on launch and after OS selection

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from ui.os_window import create_os_windows
+from ui.os_window import center_window
 
 def create_main_window():
     app = tk.Tk()
@@ -11,5 +12,7 @@ def create_main_window():
     tk.Button(app, text="Windows", command=lambda: create_os_windows(app, "Windows", "Windows"), height=2, width=20).pack(pady=10)
     tk.Button(app, text="Linux", command=lambda: create_os_windows(app, "Linux", "Linux"), height=2, width=20).pack(pady=10)
     tk.Button(app, text="Exit", command=app.destroy, height=2, width=20).pack(pady=10)
+
+    center_window(app)
 
     return app
