e8783ddc6b5439f414aa5ee048060df3d1c79745
ricsix19 <ricsix19@mailbox.unideb.hu>
2025-08-24 16:36:53 +0200

    fix(ui): Fixed center_window function error problem

--- ui/os_window.py
+++ ui/os_window.py
@@ -50,7 +50,8 @@ def back(window, app):
 
 def center_window(window):
     window.update_idletasks()
-    width, height = window.winfo.width(), window.winfo.height()
+    width = window.winfo_width()
+    height = window.winfo_height()
     x = (window.winfo_screenwidth() // 2) - (width // 2)
     y = (window.winfo_screenheight() // 2) - (height // 2)
-    window.geometry(f"{width}x{height}+{x}+{y}")
+    window.geometry('{}x{}+{}+{}'.format(width, height, x, y))
